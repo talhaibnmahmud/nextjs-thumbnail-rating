260c7be678d886a8ef83f81f3ad0ac61842fcb0c
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-03-27 15:55:09 +0600

    🚀 Setup: Convex for backend & Clerk for authentication

---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+const config =  {
+    providers: [
+        {
+            domain: 'https://classic-crab-58.clerk.accounts.dev',
+            applicationID: 'convex',
+        }
+    ],
+}
+
+export default config;
---
+++ src/providers/index.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import type { PropsWithChildren } from "react";
+
+if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+  throw new Error("Missing NEXT_PUBLIC_CONVEX_URL");
+}
+if (!process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY");
+}
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+
+export function Providers({ children }: PropsWithChildren<{}>) {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+}
